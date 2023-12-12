aa3b89b46a444001572ac9e15f164a1c136011f5
Tushar <tushargarg0987@gmail.com>
2023-12-12 16:37:21 +0530

    added: styles on hover to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client'
+
+import classnames from 'classnames'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+    const currentPath = usePathname();
+
     const links = [
         {label: 'Dashboard',href: '/'},
         {label: 'Issues',href: '/issues'}
@@ -15,7 +21,11 @@ const NavBar = () => {
                 {links.map(link => <li key={link.href}>
                     <Link
                         href={link.href}
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors font-medium'>
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}>
                         {link.label}
                     </Link>
                 </li>)}
